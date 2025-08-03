ec47aeeda7d895ed3daa98231b71d824efc085ce
Ricketrice <phanuntneang365@gmail.com>
2025-08-04 01:45:11 +1000

    Search filteration update

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const randomFoodImage = document.getElementById("randomImage");
+const FoodImage = document.getElementById("randomImage");
 const foodTitle = document.querySelector(".foodTitle");
 const ingredientList = document.querySelector(".ingredientList");
 
@@ -18,7 +18,7 @@ async function getFoodImage() {
     const response = await fetch("https://www.themealdb.com/api/json/v1/1/random.php");
     const foodData = await response.json();
     console.log(foodData);
-    randomFoodImage.src = foodData.meals[0].strMealThumb;
+    FoodImage.src = foodData.meals[0].strMealThumb;
     foodTitle.textContent = foodData.meals[0].strMeal;
     let unsupportURL = foodData.meals[0].strYoutube;
     let supportURl = unsupportURL.replace("watch?v=",  "embed/");//Iframe doesnt support watch?v=
@@ -63,7 +63,6 @@ showIntButton.addEventListener("click", (event) => {
 })
 
 
-getFoodImage();
 generateInfo.addEventListener("click", (event) => {
     event.preventDefault();
     getFoodImage();
@@ -71,4 +70,74 @@ generateInfo.addEventListener("click", (event) => {
 
 /*
 Log the return value of the data to get the data that you want. 
+*/ 
+
+//Toggle by search 
+let searchValue = document.getElementById("searchBar");
+let searchList = document.querySelector(".resultList");
+let shownResult = document.querySelector(".shownResults");
+
+async function searchByText() {
+    let value = searchValue.value;
+    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${value}`);
+    const foodData = await response.json();
+    console.log(foodData);
+    console.log(value);
+    let checkMeal = foodData.meals;
+    if (checkMeal != null) {
+        for (let i = 0; i<foodData.meals.length; i++) {
+            const listMealName = document.createElement("li");
+            listMealName.textContent = checkMeal[i].strMeal;
+            searchList.append(listMealName);
+        }
+    } else {
+        const listMealName = document.createElement("li");
+        listMealName.textContent = "No meal found";
+        searchList.append(listMealName);
+    }
+
+
+}
+
+searchByText();
+
+
+searchValue.addEventListener("input",() => {
+    searchList.innerHTML = "";
+    if (searchValue.value !== "") {
+        shownResult.style.display = "block";
+    } else {
+        shownResult.style.display = "none";
+    }
+    searchByText();
+    
+    
+})
+
+/*
+    searchValue.addEventListener("input", (event) => {
+    searchByText();
+    if (event.key == "Enter") {
+        event.preventDefault();
+        FoodImage.src = foodData.meals[0].strMealThumb;
+        foodTitle.textContent = foodData.meals[0].strMeal;
+        let unsupportURL = foodData.meals[0].strYoutube;
+        let supportURl = unsupportURL.replace("watch?v=",  "embed/");//Iframe doesnt support watch?v=
+        iframe.src = supportURl;
+        instructionDetails.textContent = foodData.meals[0].strInstructions;
+
+        
+        for (let i = 1;i<20; i++) {
+            
+            let ingredientInfo = foodData.meals[0][`strIngredient${i}`];
+            let ingredientMeasurment = foodData.meals[0][`strMeasure${i}`];
+            if (foodData.meals[0][`strIngredient${i}`] && foodData.meals[0][`strMeasure${i}`] != null || foodData.meals[0][`strIngredient1${i}`] && foodData.meals[0][`strMeasure${i}`]!= " " || foodData.meals[0][`strIngredient${i}`] && foodData.meals[0][`strMeasure${i}`] != "") {
+                const list = document.createElement("li");
+                list.textContent = foodData.meals[0][`strIngredient${i}`] + " " + foodData.meals[0][`strMeasure${i}`];
+                ingredientList.append(list);
+            }
+        }
+    }
+})
+
 */ 
